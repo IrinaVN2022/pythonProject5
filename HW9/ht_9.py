598dfa2d18fbcc815096f08eb1a015cf3da08614
iryna <gloriya.od@ukr.net>
2022-11-02 21:49:50 +0200

    task9

--- HW9/ht_9.py
+++ HW9/ht_9.py
@@ -16,7 +16,5 @@ def odd_or_even_number(number):
     return f'{number} is an Even number' if number % 2 == 0 else f"{number} is an Odd number"
 
 
-print(odd_or_even_number(89))
-
 assert odd_or_even_number(14) == '14 is an Even number'
 assert odd_or_even_number(7) == '7 is an Odd number'
