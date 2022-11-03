7464cdde79dd769a5e30d4215e4abe433a75cd90
iryna <gloriya.od@ukr.net>
2022-11-03 20:37:08 +0200

    less9

---
+++ HW9/ht_9_1_test.py
@@ -0,0 +1,6 @@
+import ht_9_1
+
+assert ht_9_1.is_string_capitalized('My name is David') is False
+assert ht_9_1.is_string_capitalized('I love playing') is True
+assert ht_9_1.is_string_capitalized('') is True
+assert ht_9_1.is_string_capitalized('565656') is True
--- HW9/test_task_1.py
+++
@@ -1,6 +0,0 @@
-import task_1
-
-assert task_1.is_string_capitalized('My name is David') is False
-assert task_1.is_string_capitalized('I love playing') is True
-assert task_1.is_string_capitalized('') is True
-assert task_1.is_string_capitalized('565656') is True
